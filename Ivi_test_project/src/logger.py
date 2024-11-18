57c28008fc8a15abdcb358452b6f4ad18dbccbcf
Arseniy Polyakov <47335403+arpoly@users.noreply.github.com>
2024-11-18 21:46:37 +0300

    Update logger.py

--- Ivi_test_project/src/logger.py
+++ Ivi_test_project/src/logger.py
@@ -1,36 +1,53 @@
+import json
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+console_handler = logging.StreamHandler()
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(console_handler)
+
+console_handler.setFormatter(logging.Formatter('%(message)s'))
 
 
 def log_request_response(response):
     request = response.request
 
-    # Формируем команду curl для запроса
-    curl_command = f"curl -X {request.method} '{request.url}'"
+    curl_command = f"{Fore.MAGENTA}curl -X {request.method} '{request.url}'"
 
-    # Добавляем заголовки
     for header, value in request.headers.items():
         curl_command += f" -H '{header}: {value}'"
-
-    # Добавляем данные запроса, если они есть
     if request.body:
         curl_command += f" -d '{request.body}'"
 
-    # Вычисляем время ответа
     elapsed_time = response.elapsed.total_seconds()
 
-    # Логируем команду curl
-    logger.info(f"Request Curl: {curl_command}")
+    logger.info(f"Request Curl: {curl_command}\n")
 
-    # Логируем данные запроса и ответа
     logger.info(f"Request Method: {request.method}")
-    logger.info(f"Request URL: {request.url}")
+    logger.info(f"Request URL: {Fore.GREEN}{request.url}{Style.RESET_ALL}")
     logger.info(f"Request Headers: {request.headers}")
-    logger.info(f"Request Json: {request.body}")
 
-    # Логирование ответа с временем
-    logger.info(f"Response Status Code: {response.status_code}")
-    logger.info(f"Response Json: {response.text}")
-    logger.info(f"Response Time: {round(elapsed_time, 3)} seconds")
+    if request.body:
+        try:
+            request_json = json.loads(request.body.decode('utf-8'))
+            formatted_request_json = json.dumps(request_json, indent=4)
+            logger.info(f"Request Json: {formatted_request_json}")
+        except (ValueError, AttributeError):
+            logger.info(f"Request Json: {request.body}")
+
+    status_color = Fore.GREEN if response.status_code == 200 else Fore.RED
+    logger.info(f"Response Status Code: {status_color}{response.status_code}{Style.RESET_ALL}")
+
+    try:
+        response_json = json.dumps(response.json(), indent=4)
+    except ValueError:
+        response_json = response.text
+
+    logger.info(f"Response Json: {response_json}")
+
+    logger.info(f"Response Time: {round(elapsed_time, 3)} seconds\n")
